86650b1b0f9afbc9d9e298ead15d0a07298c9285
EliasLAlexander <141297430+EliasLAlexander@users.noreply.github.com>
2025-03-24 23:33:39 +1030

    decide whether cache content should be wiped out and fetch to the original server or can be sent to client if still valid based on RFC 2616

--- Proxy.py
+++ Proxy.py
@@ -5,6 +5,11 @@ import os
 import argparse
 import re
 
+import email.utils
+import datetime
+from pytz import timezone
+
+
 # 1MB buffer size
 BUFFER_SIZE = 1000000
 
@@ -55,6 +60,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
+    connectionSocket, addr = serverSocket.accept() # accept connections from outside
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
@@ -64,6 +70,7 @@ while True:
   # Get HTTP request from client
   # and store it in the variable: message_bytes
   # ~~~~ INSERT CODE ~~~~
+  message_bytes = connectionSocket.recv(1024) # read bytes from socket connection
   # ~~~~ END CODE INSERT ~~~~
   message = message_bytes.decode('utf-8')
   print ('Received request:')
@@ -116,6 +123,38 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    # check if the cache is still valid by checking the status code
+    cache_status = cacheData[0].split()[1] 
+
+    # if cache is moved permanently, send the client to original server
+    if cache_status == '301':
+      # extract the new location from the cache and set the new location
+      hostname, resource = cacheLocation.split('=')[1].split('&')[0].split('/')[2], '/'
+    
+    # extract the cache control header from the cache
+    for data in cacheData:
+      # calculate the age of the cache
+      if 'Date' in data:
+        cache_time = data.split(':', 1)[1]
+        cache_age = (datetime.datetime.now(timezone('UTC')).replace(tzinfo=None) - 
+                               datetime.datetime(*email.utils.parsedate(cache_time)[:6])).total_seconds()
+      
+      # check if the cache is reusable
+      if 'Cache-Control' in data:
+        cache_control = data.split(':', 1)[1]
+        if cache_control == 'no-cache': #RFC 2616. 14.9.1
+          print('Cache is not reusable')
+          break
+      
+      # check if the cache is still valid
+      if 'max_age' in data and int(line.split(',')[1].split('=')[1]) <= cache_age:
+        print('Cache is expired')
+        break
+
+      # use connection socket to send the cache data to the client
+      else:
+        [connectionSocket.send(data) for data in cacheData]
+        exit()
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
@@ -126,6 +165,7 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+    originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -134,6 +174,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address, 80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
@@ -144,10 +185,12 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
+      originServerRequestLine = method + ' ' + resource + ' ' + version
+      originServerRequestHeader = 'Host: ' + hostname + '\r\n'
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
-      request = originServerRequest + '\r\n' + originServerRequestHeader + '\r\n\r\n'
+      request = originServerRequestLine + '\r\n' + originServerRequestHeader + '\r\n\r\n'
 
       # Request the web resource from origin server
       print ('Forwarding request to origin server:')
@@ -164,10 +207,13 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
+      response = originServerSocket.recv(BUFFER_SIZE)
+
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      connectionSocket.send(response)
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -179,6 +225,8 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      for data in response:
+        cacheFile.write(data)
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
