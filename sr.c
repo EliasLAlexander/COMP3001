667bb7229cbc59e688363871f3e350ff43dfdeb3
EliasLAlexander <141297430+EliasLAlexander@users.noreply.github.com>
2025-04-20 11:16:57 +0930

    "modify TRACE check"

--- sr.c
+++ sr.c
@@ -118,7 +118,7 @@ void A_input(struct pkt packet)
   /* if received ACK is not corrupted */ 
   if (!IsCorrupted(packet)) {
     if (TRACE > 0)
-      printf("----B: packet %d is correctly received, send ACK!\n",packet.acknum);
+      printf("----A: uncorrupted ACK %d is received\n" ,packet.acknum);
     total_ACKs_received++; /* increase ACK and RESTART timer */
 
     /* check if new ACK or duplicate */
@@ -183,15 +183,14 @@ void A_timerinterrupt(void)
 
   /* if window is not empty, resend the packet not ACKed in window */
   for (i=0; i<WINDOWSIZE; i++) {
-    int index = (windowfirst + i) % WINDOWSIZE;
 
-    if (buffer[index].acknum != 0) {
+    if (buffer[(windowfirst + i) % WINDOWSIZE].acknum != 0) {
       if (TRACE > 0)
-        printf("----A: resending packet %d\n", buffer[index].seqnum);
+        printf("----A: resending packet %d\n", (buffer[(windowfirst + i) % WINDOWSIZE]).seqnum);
     }
 
     /* resend the packet to layer3 */
-    tolayer3(A,buffer[index]);
+    tolayer3(A,buffer[(windowfirst + i) % WINDOWSIZE]);
     packets_resent++;
 
     /* start timer for resent packet */
